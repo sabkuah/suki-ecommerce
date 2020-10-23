1abbb513c24374ee38597491daedc190890551f0
cambateman <cambateman604@gmail.com>
2020-10-23 12:19:37 -0700

    Dynamic creation of cards

--- scripts/products.js
+++ scripts/products.js
@@ -1,9 +1,13 @@
-var productName = "Splitboard"
-document.getElementById("product-row-1").innerHTML = `<div class='col-sm-3'>
-<div class='card'>
-    <img class='card-img-top img-fluid' src='img/snowboard.jpeg' alt='Card image cap'>
-    <div class='card-block'>
-        <h4 class='card-title'>${productName}</h4>
+var productNames = ["Banana Split", "Skis"] 
+var itemDiv = ""
+productNames.forEach(type => {
+    itemDiv = itemDiv + `<div class='col-sm-3'>
+    <div class='card'>
+        <img class='card-img-top img-fluid' src='img/snowboard.jpeg' alt='Card image cap'>
+        <div class='card-block'>
+            <h4 class='card-title'>${type}</h4>
+        </div>
     </div>
-</div>
-</div>`
+    </div>`  
+})
+document.getElementById("product-row-1").innerHTML = itemDiv
