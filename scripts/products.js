c2b82077e03739461378197d8c8f8a316fc874b0
Cam Bateman <Cam@Users-MacBook-Pro.local>
2020-10-23 11:47:56 -0700

    Add products.js file

---
+++ scripts/products.js
@@ -0,0 +1,9 @@
+var productName = "Splitboard"
+document.getElementById("product-row-1").innerHTML = `<div class='col-sm-3'>
+<div class='card'>
+    <img class='card-img-top img-fluid' src='img/snowboard.jpeg' alt='Card image cap'>
+    <div class='card-block'>
+        <h4 class='card-title'>${productName}</h4>
+    </div>
+</div>
+</div>`
