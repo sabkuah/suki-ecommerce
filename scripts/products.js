a54efbff21864fcea18726847f2bdbbfddb369b9
cambateman <cambateman604@gmail.com>
2020-10-23 16:07:03 -0700

    pushing broken script  for repair

--- scripts/products.js
+++ scripts/products.js
@@ -8,21 +8,23 @@ let item = new Product("Banana Split", "./img/snowboard-2.jpg", 500.00, "snowboa
 
 
 
-var productNames = [item, { name: "skis", image: "./img/snowboard-1.jpg" }]
+var productNames = [item, {name: "skis", image: "./img/snowboard-1.jpg" }]
 var itemDiv = ""
 
 productNames.forEach(product => {
     console.log(product.name)
     itemDiv = itemDiv + `<div class='col-sm-3'>
-    <a href="details.html" class="purchase-link">
-    <div class='card'>
-        <img class='card-img-top img-fluid' src=${product.image} alt='Card image cap'>
-        <div class='card-block'>
-            <h4 class='card-title'>${product.name}</h4>
+    <a href="details.html" class="purchase-link" onclick="()=>{
+        addItemToStorage('selectedItem', new Product('${product.name}', '${product.image}', '${product.price}', '${product.type}')
+    }">
+        <div class='card'>
+            <img class='card-img-top img-fluid' src=${product.image} alt='Card image cap'>
+            <div class='card-block'>
+                <h4 class='card-title'>${product.name}</h4>
+            </div>
         </div>
-    </div>
-    </a>
-</div>`
+        </a>
+    </div>`
 })
 document.getElementById("product-row-1").innerHTML = itemDiv
 
@@ -33,4 +35,4 @@ var itemInCart = ""
 
 
 
-document.getElementById("product-added").innerHTML = itemInCart
+//document.getElementById("product-added").innerHTML = itemInCart
