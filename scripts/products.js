676c0cee72f0e64560dd8252dce1cd464fb3068c
Stefan Kuznetsov <skuznetsov2@my.bcit.ca>
2020-10-23 16:24:27 -0700

    Fixed the onclick

--- scripts/products.js
+++ scripts/products.js
@@ -1,9 +1,3 @@
-function Product(name, image, price, type) {
-    this.name = name;
-    this.image = image;
-    this.price = price;
-    this.type = type;
-}
 let item = new Product("Banana Split", "./img/snowboard-2.jpg", 500.00, "snowboard")
 
 
@@ -14,9 +8,8 @@ var itemDiv = ""
 productNames.forEach(product => {
     console.log(product.name)
     itemDiv = itemDiv + `<div class='col-sm-3'>
-    <a href="details.html" class="purchase-link" onclick="()=>{
-        addItemToStorage('selectedItem', new Product('${product.name}', '${product.image}', '${product.price}', '${product.type}')
-    }">
+    <a href="details.html" class="purchase-link" onclick="
+        addItemToStorage('selectedItem', new Product('${product.name}', '${product.image}', '${product.price}', '${product.type}'));">
         <div class='card'>
             <img class='card-img-top img-fluid' src=${product.image} alt='Card image cap'>
             <div class='card-block'>
